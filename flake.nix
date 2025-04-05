e7779877ad2f129e3c95f6d481468d3689d4e743
NewDawn0 <newdawn.v0.0+git@gmail.com>
2025-04-06 00:28:02 +1000

    update nix utils

--- flake.nix
+++ flake.nix
@@ -1,41 +1,30 @@
 {
-  description = "Your awesome flake";
+  description = "Runnable VSCode config";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
-    utils = {
-      url = "github:NewDawn0/nixUtils";
-      inputs.nixpkgs.follows = "nixpkgs";
+  inputs.utils.url = "github:NewDawn0/nixUtils";
+
+  outputs = { self, utils, ... }: {
+    overlays.default = final: prev: {
+      ndvscode = self.packages.${prev.system}.default;
+      ndvscode-config = self.packages.${prev.system}.ndvscode-config;
     };
-  };
-
-  outputs = { nixpkgs, self, utils, ... }:
-    let
-      mkPkgs = { system, ... }:
-        import nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              vscode-extensions = prev.vscode-extensions
-                // (import ./overlays.nix { pkgs = prev; });
-            })
-          ];
-          config.allowUnfree = true;
+    packages = utils.lib.eachSystem {
+      config = { allowUnfree = true; };
+      overlays = [
+        (final: prev: {
+          vscode-extensions = prev.vscode-extensions
+            // (import ./overlays.nix { pkgs = prev; });
+        })
+      ];
+    } (pkgs:
+      let
+        ndvscode-config = import ./cfg/config.nix { inherit pkgs; };
+        ndvscode = pkgs.vscode-with-extensions.override {
+          vscodeExtensions = import ./cfg/plugins.nix { inherit pkgs; };
         };
-    in {
-      overlays.default = final: prev: {
-        ndvscode = self.packages.${prev.system}.default;
-        ndvscode-config = self.packages.${prev.system}.ndvscode-config;
-      };
-      packages = utils.lib.eachSystem { inherit mkPkgs; } (pkgs:
-        let
-          ndvscode-config = import ./cfg/config.nix { inherit pkgs; };
-          ndvscode = pkgs.vscode-with-extensions.override {
-            vscodeExtensions = import ./cfg/plugins.nix { inherit pkgs; };
-          };
-        in {
-          inherit ndvscode-config ndvscode;
-          default = ndvscode;
-        });
-    };
+      in {
+        inherit ndvscode-config ndvscode;
+        default = ndvscode;
+      });
+  };
 }
